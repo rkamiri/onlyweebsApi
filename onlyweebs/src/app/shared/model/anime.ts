df9456afe9ad5f95e7107ad11f350edeabb53340
rkamiri <rkamiri@iut.univ-paris8.fr>
2021-03-19 11:53:40 +0100

    cleaned project

--- onlyweebs/src/app/shared/model/anime.ts
+++
@@ -1,8 +0,0 @@
-export interface Anime {
-    id: number;
-    title: string;
-    internationalTitle: string;
-    synopsis: string;
-    cover: string;
-    ranking: number;
-}
--- onlyweebs/src/app/shared/model/lists.ts
+++
@@ -1,6 +0,0 @@
-export interface Lists {
-    id: number;
-    name: string;
-    creationDate: string;
-    description: string;
-}
