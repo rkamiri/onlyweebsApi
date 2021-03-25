7cde823fbac4b2d9ac1144282130ad39781c22c1
nlmz94 <nmz94140@gmail.com>
2021-03-25 15:21:37 +0000

    modif cors

--- src/main/java/fr/paris8univ/iut/csid/csidwebrepositorybase/CsidWebRepositoryBaseApplication.java
+++ src/main/java/fr/paris8univ/iut/csid/csidwebrepositorybase/CsidWebRepositoryBaseApplication.java
@@ -2,14 +2,21 @@ package fr.paris8univ.iut.csid.csidwebrepositorybase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class CsidWebRepositoryBaseApplication {
+public class CsidWebRepositoryBaseApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(CsidWebRepositoryBaseApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(CsidWebRepositoryBaseApplication.class);
     }
 
     @Bean
