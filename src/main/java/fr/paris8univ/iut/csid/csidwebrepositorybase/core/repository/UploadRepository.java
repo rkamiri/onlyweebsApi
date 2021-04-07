cecc900c045a9f9ff65cfb5d45c9390e79ead44f
nlmz94 <nmz94140@gmail.com>
2021-04-07 17:33:54 +0200

    Images are now resized 150*150 to save space.

--- src/main/java/fr/paris8univ/iut/csid/csidwebrepositorybase/core/repository/UploadRepository.java
+++ src/main/java/fr/paris8univ/iut/csid/csidwebrepositorybase/core/repository/UploadRepository.java
@@ -7,9 +7,10 @@ import fr.paris8univ.iut.csid.csidwebrepositorybase.core.entity.ImageEntity;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
 import java.util.Optional;
 
 @Repository
@@ -25,22 +26,36 @@ public class UploadRepository {
 
     public void saveImage(MultipartFile serverFile, Long userid) throws IOException {
         ImageEntity ie = new ImageEntity();
-        ie.setContent(serverFile.getBytes());
+        ie.setContent(scale(serverFile.getBytes(), 150, 150));
         ie.setName(serverFile.getOriginalFilename());
         this.imageDao.save(ie);
-        Long imageid = 0L;
+        Long imageId = 0L;
         for (ImageEntity e:this.imageDao.findAll()) {
-            if (e.getId()>imageid)
-                imageid = e.getId();
+            if (e.getId()>imageId)
+                imageId = e.getId();
         }
         if (this.hasImageDao.findById(userid).isPresent()) {
             HasImageEntity hie = this.hasImageDao.findById(userid).get();
-            hie.setImageid(imageid);
+            hie.setImageid(imageId);
             this.hasImageDao.save(hie);
         }
     }
 
     public ImageEntity findById(Long imageId) {
         return this.imageDao.findById(imageId).orElseGet(ImageEntity::new);
+    }
+
+    public byte[] scale(byte[] fileData, int width, int height) {
+        ByteArrayInputStream in = new ByteArrayInputStream(fileData);
+        try {
+            BufferedImage img = ImageIO.read(in);
+            Image scaledImage = img.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+            BufferedImage imageBuff = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+            imageBuff.getGraphics().drawImage(scaledImage, 0, 0, new Color(0,0,0), null);
+            ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+            ImageIO.write(imageBuff, "jpg", buffer);
+            return buffer.toByteArray();
+        } catch (IOException e) {}
+        return new byte[10];
     }
 }
