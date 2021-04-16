082e45de162eca95e3415de7da07b403a3c7cbe9
nlmz94 <46894034+nlmz94@users.noreply.github.com>
2021-04-16 16:21:40 +0200

    Tmp comment branch (#13)
    * put anime in existing list + delete anime from existing list
    
    * Lists are now characterised by their owner
    
    * find a list by its name + owner id
    
    * changed how we call for lists + show them, added default lists
    
    * back to windows motherfucker
    
    * lists are now categorised as default or custom
    
    * plz work
    
    Co-authored-by: rkamiri <rkamiri@iut.univ-paris8.fr>

--- src/main/java/fr/paris8univ/iut/csid/csidwebrepositorybase/core/repository/AnimeCommentRepository.java
+++ src/main/java/fr/paris8univ/iut/csid/csidwebrepositorybase/core/repository/AnimeCommentRepository.java
@@ -7,6 +7,8 @@ import fr.paris8univ.iut.csid.csidwebrepositorybase.core.model.AnimeCommentId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,6 +17,7 @@ public class AnimeCommentRepository {
 
     private final AnimeCommentDao animeCommentDao;
     public final UsersRepository ur;
+    private DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
     @Autowired
     public AnimeCommentRepository(AnimeCommentDao animeCommentDao, UsersRepository ur) {
@@ -23,7 +26,18 @@ public class AnimeCommentRepository {
     }
 
     public void putAComment(AnimeComment comment)  {
-        this.animeCommentDao.save(new AnimeCommentEntity(comment.getUserId(), comment.getAnimeId(), comment.getComment()));
+        List<AnimeCommentEntity> animeCommentEntities = this.animeCommentDao.findAll();
+        animeCommentEntities.removeIf(x -> !x.getUser_id().equals(comment.getUser_id()));
+        animeCommentEntities.removeIf(x -> !x.getAnime_id().equals(comment.getAnime_id()));
+        LocalDateTime now = LocalDateTime.now();
+        if(animeCommentEntities.size()>0){
+            animeCommentEntities.get(0).setComment(comment.getComment());
+            animeCommentEntities.get(0).setDate(dtf.format(now));
+            animeCommentDao.save(animeCommentEntities.get(0));
+        }
+        else{
+            this.animeCommentDao.save(new AnimeCommentEntity(comment.getUser_id(), comment.getAnime_id(), comment.getComment(),dtf.format(now)));
+        }
     }
 
     public List<AnimeCommentEntity> getAllCommentsByAnimeId(long animeid) {
