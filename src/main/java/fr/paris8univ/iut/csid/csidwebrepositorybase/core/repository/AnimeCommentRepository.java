9a753254b9f159f96324d214230c237433b49024
nlmz94 <nmz94140@gmail.com>
2021-05-03 19:26:23 +0200

    Refactored code for better comment management

--- src/main/java/fr/paris8univ/iut/csid/csidwebrepositorybase/core/repository/AnimeCommentRepository.java
+++ src/main/java/fr/paris8univ/iut/csid/csidwebrepositorybase/core/repository/AnimeCommentRepository.java
@@ -1,7 +1,9 @@
 package fr.paris8univ.iut.csid.csidwebrepositorybase.core.repository;
 
+import fr.paris8univ.iut.csid.csidwebrepositorybase.core.controller.UserController;
 import fr.paris8univ.iut.csid.csidwebrepositorybase.core.dao.AnimeCommentDao;
 import fr.paris8univ.iut.csid.csidwebrepositorybase.core.entity.AnimeCommentEntity;
+import fr.paris8univ.iut.csid.csidwebrepositorybase.core.entity.UsersEntity;
 import fr.paris8univ.iut.csid.csidwebrepositorybase.core.model.AnimeComment;
 import fr.paris8univ.iut.csid.csidwebrepositorybase.core.model.AnimeCommentId;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,43 +18,43 @@ import java.util.Optional;
 public class AnimeCommentRepository {
 
     private final AnimeCommentDao animeCommentDao;
-    public final UsersRepository ur;
-    private DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    public final UsersRepository usersRepository;
+    private final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
     @Autowired
-    public AnimeCommentRepository(AnimeCommentDao animeCommentDao, UsersRepository ur) {
+    public AnimeCommentRepository(AnimeCommentDao animeCommentDao, UsersRepository usersRepository) {
         this.animeCommentDao = animeCommentDao;
-        this.ur = ur;
+        this.usersRepository = usersRepository;
     }
 
-    public void putAComment(AnimeComment comment)  {
+    public void putAComment(AnimeComment comment) {
+        UsersEntity user = this.usersRepository.findUserEntityByUsername(UserController.getCurrentUserLogin());
         List<AnimeCommentEntity> animeCommentEntities = this.animeCommentDao.findAll();
-        animeCommentEntities.removeIf(x -> !x.getUser_id().equals(comment.getUser_id()));
+        animeCommentEntities.removeIf(x -> !x.getUsersEntity().getId().equals(user.getId()));
         animeCommentEntities.removeIf(x -> !x.getAnime_id().equals(comment.getAnime_id()));
         LocalDateTime now = LocalDateTime.now();
-        if(animeCommentEntities.size()>0){
+        if (animeCommentEntities.size() > 0) {
             animeCommentEntities.get(0).setComment(comment.getComment());
             animeCommentEntities.get(0).setDate(dtf.format(now));
             animeCommentDao.save(animeCommentEntities.get(0));
-        }
-        else{
-            this.animeCommentDao.save(new AnimeCommentEntity(comment.getUser_id(), comment.getAnime_id(), comment.getComment(),dtf.format(now)));
+        } else {
+            this.animeCommentDao.save(
+                    new AnimeCommentEntity(
+                            user,
+                            comment.getAnime_id(), comment.getComment(),
+                            dtf.format(now)));
         }
     }
 
-    public List<AnimeCommentEntity> getAllCommentsByAnimeId(long animeid) {
+    public List<AnimeCommentEntity> getAllCommentsByAnimeId(long animeId) {
         List<AnimeCommentEntity> comments = this.animeCommentDao.findAll();
-        comments.removeIf(entity -> !entity.getAnime_id().equals(animeid));
+        comments.removeIf(entity -> !entity.getAnime_id().equals(animeId));
         return comments;
     }
 
-    public AnimeCommentDao getAnimeCommentDao() {
-        return animeCommentDao;
-    }
-
-    public Optional<AnimeComment> getCurrentUserAnimeCommentForASelectAnime(String currentUserLogin, Long animeid) {
-        if (ur.findByUsername(currentUserLogin).isPresent()) {
-            return this.animeCommentDao.findById(new AnimeCommentId(ur.findByUsername(currentUserLogin).get().getId(), animeid)).map(AnimeComment::new);
+    public Optional<AnimeComment> getCurrentUserAnimeCommentForASelectAnime(String currentUserLogin, Long animeId) {
+        if (usersRepository.findByUsername(currentUserLogin).isPresent()) {
+            return this.animeCommentDao.findById(new AnimeCommentId(usersRepository.findByUsername(currentUserLogin).get().getId(), animeId)).map(AnimeComment::new);
         } else {
             return Optional.empty();
         }
