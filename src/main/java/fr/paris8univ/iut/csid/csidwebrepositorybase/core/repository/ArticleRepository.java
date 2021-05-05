7a0692fe5861076e429602d019e6e697698cd6c9
nlmz94 <nmz94140@gmail.com>
2021-05-05 14:00:02 +0200

    Post an article

--- src/main/java/fr/paris8univ/iut/csid/csidwebrepositorybase/core/repository/ArticleRepository.java
+++ src/main/java/fr/paris8univ/iut/csid/csidwebrepositorybase/core/repository/ArticleRepository.java
@@ -1,9 +1,7 @@
 package fr.paris8univ.iut.csid.csidwebrepositorybase.core.repository;
 
 import fr.paris8univ.iut.csid.csidwebrepositorybase.core.dao.ArticleDao;
-import fr.paris8univ.iut.csid.csidwebrepositorybase.core.entity.AnimeEntity;
 import fr.paris8univ.iut.csid.csidwebrepositorybase.core.entity.ArticleEntity;
-import fr.paris8univ.iut.csid.csidwebrepositorybase.core.model.Anime;
 import fr.paris8univ.iut.csid.csidwebrepositorybase.core.model.Article;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -28,5 +26,30 @@ public class ArticleRepository {
 
     public Article getArticle(long id) {
         return new Article(this.articleDao.getOne(id));
+    }
+
+    public Long postArticle(Article article) {
+        this.articleDao.save(
+                new ArticleEntity(
+                        article.getTitle(),
+                        article.getBody(),
+                        article.getCreated_at(),
+                        article.getAuthor(),
+                        article.getCover()
+                ));
+        return this.getLastArticleId();
+    }
+
+    public Long getLastArticleId() {
+        List<ArticleEntity> ael = this.articleDao.findAll();
+        if (ael.size() > 0) {
+            Long id = 1L;
+            for (ArticleEntity ae : ael) {
+                if (ae.getId() >= id) id = ae.getId();
+            }
+            return this.articleDao.getOne(id).getId();
+        } else {
+            return 0L;
+        }
     }
 }
