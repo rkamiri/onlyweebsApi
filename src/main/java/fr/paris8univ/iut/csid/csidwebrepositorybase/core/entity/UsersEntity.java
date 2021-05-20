90a124338265d7ddec864b423be9218a96b208d7
nlmz94 <nmz94140@gmail.com>
2021-05-20 19:40:16 +0200

    remove duplicate getter/setter

--- src/main/java/fr/paris8univ/iut/csid/csidwebrepositorybase/core/entity/UsersEntity.java
+++ src/main/java/fr/paris8univ/iut/csid/csidwebrepositorybase/core/entity/UsersEntity.java
@@ -128,14 +128,6 @@ public class UsersEntity {
         this.ip = ip;
     }
 
-    public String getIp() {
-        return ip;
-    }
-
-    public void setIp(String ip) {
-        this.ip = ip;
-    }
-
     public ImageEntity getImage() {
         return image;
     }
