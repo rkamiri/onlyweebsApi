5ded1596a461cb023d51f141b3622f641ac8aa75
nlmz94 <nmz94140@gmail.com>
2021-05-21 16:34:50 +0200

    ssl 3nd try port change again

--- src/main/java/fr/paris8univ/iut/csid/csidwebrepositorybase/core/service/MailService.java
+++ src/main/java/fr/paris8univ/iut/csid/csidwebrepositorybase/core/service/MailService.java
@@ -26,9 +26,9 @@ public class MailService {
     private void setMailServerProperties() {
         Properties emailProperties = System.getProperties();
         emailProperties.put("mail.smtp.host", "smtp.gmail.com");
-        emailProperties.put("mail.smtp.starttls.enable", "true");
         emailProperties.put("mail.smtp.port", "587");
         emailProperties.put("mail.smtp.auth", "true");
+        emailProperties.put("mail.smtp.starttls.enable", "true");
         mailSession = Session.getInstance(emailProperties,
                 new javax.mail.Authenticator() {
                     protected PasswordAuthentication getPasswordAuthentication() {
