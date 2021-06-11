2dd5c301870510ebf5d587ac4f915c5b073da8a9
rkamiri <rkamiri@iut.univ-paris8.fr>
2021-06-11 12:06:31 +0200

    test with new query

--- src/main/java/fr/paris8univ/iut/csid/csidwebrepositorybase/core/dao/ArticleDao.java
+++ src/main/java/fr/paris8univ/iut/csid/csidwebrepositorybase/core/dao/ArticleDao.java
@@ -18,18 +18,20 @@ import java.util.List;
 @Repository
 public interface ArticleDao extends JpaRepository<ArticleEntity, Long> {
     List<ArticleEntity> getArticleEntitiesByAuthor(UsersEntity author);
+
     List<ArticleEntity> getArticleEntitiesByCategoryIdAndTitleLike(Pageable page, Long category, String title);
 
-
-    /*@Query(value = "Select distinct a.id, a.title, a.body,a.created_at, a.author_id, a.cover_id, a.category_id from article a  " +
-            "where (:query is null or title like CONCAT('%',:query,'%')) and (:category is null or category_id = :category),
-            countQuery = "select count(*) from article a  " +
-                         "where (:query is null or title like CONCAT('%',:query,'%')) and (:category is null or category_id = :category)",
-            nativeQuery = true)*/
     @Query(value = "Select distinct a.id, a.title, a.body, a.created_at, a.author_id, a.cover_id, a.category_id from article a" +
-            "where (:query is null or title like CONCAT('%',:query,'%')) and (:category is null or category_id = :category),
-            )
+            "where (:query is null or title like CONCAT('%',:query,'%')) and (:category is null or category_id = :category)",
 
-    Page<ArticleEntity> findAllByCompleteResearch(@Param("query")String query, @Param("category") String producer, Pageable page);
+
+
+
+
+            countQuery = "select count(*) from article a " +
+                    "where (:query is null or title like CONCAT('%',:query,'%')) and (:category is null or category_id = :category)",
+            nativeQuery = true)
+
+    Page<ArticleEntity> findAllByCompleteResearch(@Param("query")String query, @Param("category") Long category, Pageable page);
 
 }
