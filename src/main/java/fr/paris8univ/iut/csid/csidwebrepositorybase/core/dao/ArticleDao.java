d004c9b031c92dd2a38d027ae0b5a6eb14803101
rkamiri <rkamiri@iut.univ-paris8.fr>
2021-06-11 11:55:14 +0200

    new query

--- src/main/java/fr/paris8univ/iut/csid/csidwebrepositorybase/core/dao/ArticleDao.java
+++ src/main/java/fr/paris8univ/iut/csid/csidwebrepositorybase/core/dao/ArticleDao.java
@@ -1,12 +1,16 @@
 package fr.paris8univ.iut.csid.csidwebrepositorybase.core.dao;
 
+import fr.paris8univ.iut.csid.csidwebrepositorybase.core.entity.AnimeEntity;
 import fr.paris8univ.iut.csid.csidwebrepositorybase.core.entity.ArticleCategoriesEntity;
 import fr.paris8univ.iut.csid.csidwebrepositorybase.core.entity.ArticleEntity;
 import fr.paris8univ.iut.csid.csidwebrepositorybase.core.entity.UsersEntity;
 import fr.paris8univ.iut.csid.csidwebrepositorybase.core.model.Article;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,4 +19,17 @@ import java.util.List;
 public interface ArticleDao extends JpaRepository<ArticleEntity, Long> {
     List<ArticleEntity> getArticleEntitiesByAuthor(UsersEntity author);
     List<ArticleEntity> getArticleEntitiesByCategoryIdAndTitleLike(Pageable page, Long category, String title);
+
+
+    /*@Query(value = "Select distinct a.id, a.title, a.body,a.created_at, a.author_id, a.cover_id, a.category_id from article a  " +
+            "where (:query is null or title like CONCAT('%',:query,'%')) and (:category is null or category_id = :category),
+            countQuery = "select count(*) from article a  " +
+                         "where (:query is null or title like CONCAT('%',:query,'%')) and (:category is null or category_id = :category)",
+            nativeQuery = true)*/
+    @Query(value = "Select distinct a.id, a.title, a.body, a.created_at, a.author_id, a.cover_id, a.category_id from article a" +
+            "where (:query is null or title like CONCAT('%',:query,'%')) and (:category is null or category_id = :category),
+            )
+
+    Page<ArticleEntity> findAllByCompleteResearch(@Param("query")String query, @Param("category") String producer, Pageable page);
+
 }
