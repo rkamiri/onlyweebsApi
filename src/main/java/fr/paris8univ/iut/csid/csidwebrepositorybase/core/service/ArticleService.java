da91feb7f9ec34952f38f18029d6908bde20f5b9
nlmz94 <46894034+nlmz94@users.noreply.github.com>
2021-06-24 10:52:51 +0200

    Admin can delete an article and all comments of said article (#53) (#54)

--- src/main/java/fr/paris8univ/iut/csid/csidwebrepositorybase/core/service/ArticleService.java
+++ src/main/java/fr/paris8univ/iut/csid/csidwebrepositorybase/core/service/ArticleService.java
@@ -1,11 +1,9 @@
 package fr.paris8univ.iut.csid.csidwebrepositorybase.core.service;
 
-import fr.paris8univ.iut.csid.csidwebrepositorybase.core.entity.ArticleEntity;
 import fr.paris8univ.iut.csid.csidwebrepositorybase.core.model.Article;
 import fr.paris8univ.iut.csid.csidwebrepositorybase.core.repository.ArticleRepository;
 import javassist.NotFoundException;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -30,15 +28,19 @@ public class ArticleService {
         return this.articleRepository.postArticle(article);
     }
 
-    public List<Article> getArticlesByCategoryId (int page, String query, Integer categoryId){
+    public List<Article> getArticlesByCategoryId(int page, String query, Integer categoryId) {
         return this.articleRepository.getArticlesByCategoryId(page, query, categoryId).stream().map(Article::new).collect(Collectors.toList());
     }
 
-    public List<Article> getArticlesByPage (int page){
+    public List<Article> getArticlesByPage(int page) {
         return this.articleRepository.getArticlesByPage(page).stream().map(Article::new).collect(Collectors.toList());
     }
 
-    public List<Article> getSimilarArticles(long category, long articleId){
+    public List<Article> getSimilarArticles(long category, long articleId) {
         return this.articleRepository.getSimilarArticles(category, articleId).stream().map(Article::new).collect(Collectors.toList());
+    }
+
+    public void deleteArticle(long id) {
+        this.articleRepository.deleteArticle(id);
     }
 }
