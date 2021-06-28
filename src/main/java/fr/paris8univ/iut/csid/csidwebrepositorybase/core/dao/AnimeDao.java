a04d841f34bb4e4a1f1b32de8e0a695963d9d555
nlmz94 <46894034+nlmz94@users.noreply.github.com>
2021-06-28 11:57:56 +0200

    Update pagination with more params (#63)
    * added possibility to search anime by producers, studios, and genre when previously only title was available
    
    * Dev (#57)
    
    * yup (#55)
    
    * Admin can delete an article and all comments of said article (#53) (#54)
    
    * updated how we get articles for main page
    
    * added an endpoint to get users by their id.... (#56)
    
    * Dev (#58)
    
    * yup (#55)
    
    * Admin can delete an article and all comments of said article (#53) (#54)
    
    * updated how we get articles for main page
    
    * added an endpoint to get users by their id.... (#56)
    
    * added more logic to list images
    
    * minor fix advanced research of anime
    
    * Dev (#60)
    
    * yup (#55)
    
    * Admin can delete an article and all comments of said article (#53) (#54)
    
    * updated how we get articles for main page
    
    * added an endpoint to get users by their id.... (#56)
    
    * added more logic to list images
    
    * Feature stats page (#59)
    
    * Created many endpoints for preparations for stats of the website
    
    * improved AnimeStats
    
    * improved performance when fetching and parsing lists
    
    * fixed a bug when calculating some stats
    
    Co-authored-by: rkamiri <rkamiri@iut.univ-paris8.fr>
    Co-authored-by: Decnox <loick.ca@hotmail.fr>

--- src/main/java/fr/paris8univ/iut/csid/csidwebrepositorybase/core/dao/AnimeDao.java
+++ src/main/java/fr/paris8univ/iut/csid/csidwebrepositorybase/core/dao/AnimeDao.java
@@ -5,8 +5,9 @@ import fr.paris8univ.iut.csid.csidwebrepositorybase.core.entity.PegiEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 @Repository
@@ -16,13 +17,24 @@ public interface AnimeDao extends JpaRepository<AnimeEntity, Long> {
 
     List<AnimeEntity> findTop15ByTitleContainingAndPegiEntityNotLike(String researchInternationalTitle, PegiEntity pegiEntity);
 
-    List<AnimeEntity> findByTitleContainingAndPegiEntityNotLike(String researchInternationalTitle, PegiEntity pegiEntity);
-
     Page<AnimeEntity> findAllByPegiEntityNotLike(Pageable page, PegiEntity pegiEntity);
 
+    @Query(value = "Select distinct a.aired, a.airing, a.episodes,a.id, a.img_url, a.pegi_id, a.synopsis, a.title, a.title_english from anime a  " +
+            "inner join has_studio s on a.id=s.id_anime inner join has_producer p on a.id=p.id_anime inner join has_genre g on a.id=g.id " +
+            "where (:producer is null or id_producer = :producer) and (:studio is null or id_studio = :studio) and (:genre is null or id_genre = :genre) and pegi_id != 6",
+            countQuery = "select count(*) from anime a  " +
+                    "inner join has_studio s on a.id=s.id_anime inner join has_producer p on a.id=p.id_anime inner join has_genre g on a.id=g.id " +
+                    "where (:producer is null or id_producer = :producer) and (:studio is null or id_studio = :studio) and (:genre is null or id_genre = :genre) and pegi_id != 6",
+            nativeQuery = true)
+    Page<AnimeEntity> findAllByCompleteResearch(@Param("producer") Long producer, @Param("studio") Long studio, @Param("genre") Long genre, Pageable page);
+
+    @Query(value = "Select distinct a.aired, a.airing, a.episodes,a.id, a.img_url, a.pegi_id, a.synopsis, a.title, a.title_english from anime a  " +
+            "inner join has_studio s on a.id=s.id_anime inner join has_producer p on a.id=p.id_anime inner join has_genre g on a.id=g.id " +
+            "where (:producer is null or id_producer = :producer) and (:studio is null or id_studio = :studio) and (:genre is null or id_genre = :genre) and pegi_id != 6",
+            nativeQuery = true)
+    List<AnimeEntity> findAllByCompleteResearchForCount(@Param("producer") Long producer, @Param("studio") Long studio, @Param("genre") Long genre);
     Page<AnimeEntity> findByTitleContainingAndPegiEntityNotLike(Pageable page, String researchInternationalTitle, PegiEntity hentaiEntity);
-
     List<AnimeEntity> findTop15ByAiringIsContainingAndPegiEntityNotLikeOrderByAiringDesc(String date, PegiEntity pegiEntity);
-
     long count();
+    List<AnimeEntity> findByTitleContainingAndPegiEntityNotLike(String research, PegiEntity hentaiEntity);
 }
